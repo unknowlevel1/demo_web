cd65d7ffde115fba7a3ead3b8d4e1c4020be43c5
unknowlevel1 <matthewnino81@gmail.com>
2025-12-26 12:50:04 -0900

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,16 @@
 <?php
-       session_start();
+session_start();
 
-       $host = "webpractice-server.mysql.database.azure.com";
-       $username = "brksyjredx";
-       $password = "SeLfJTxN9ki2W$FY";
-       $database = "webpractice-database";
-       $conn = new mysqli($host, $username, $password, $database);
+    $host = "webpractice-server.mysql.database.azure.com";
+    $username = "brksyjredx";
+    $password = "SeLfJTxN9ki2W$FY";
+    $database = "webpractice-database";
+    $conn = new mysqli($host, $username, $password, $database);
 
-       if (!$conn) {                                             
+    if (!$conn) {                                             
            die("Connection failed: " . mysqli_connect_error());     
-       }
+    }
    
-
     $Input_user = $_POST['username'];
     $Input_pass = $_POST['password'];
 
@@ -19,6 +18,7 @@
 
     $result = mysqli_query($conn, $sql);
     $count = mysqli_num_rows($result);
+
     while ($data = mysqli_fetch_array($result)){
          $user_id = $data['Id'];
          $user_name = $data['username'];
@@ -32,11 +32,6 @@
     }else{
            header('Location: login.html');
     }
-
-         $conn -> close();
-}
-
-
 
 ?>
 
