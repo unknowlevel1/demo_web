3ef885f464aab0ffaf32cb81449ef2550420e1d1
unknowlevel1 <matthewnino81@gmail.com>
2025-12-26 09:14:15 -0900

    Update create.php

--- create.php
+++ create.php
@@ -1,34 +1,27 @@
 <?php
 
-      $host = '';
-      //echo $username = '';
-      $username = '';
-      $password = '';
-      $db_name = '';
+   //echo $username = '';
+    $host = "webpractice-server.mysql.database.azure.com";
+    $username = "brksyjredx";
+    $password = "SeLfJTxN9ki2W$FY";
+    $database = "webpractice-database";
       
       $conn = mysqli_init();
 
-          mysqli_ssl_set($conn, NULL, NULL,"D:\study\DigiCertGlobalRootCA.crt",NULL,NULL);
-          mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
-          if (mysqli_connect_errno($conn)) {
-          die('Failed to connect to MySQL: '.mysqli_connect_error());
+      mysqli_ssl_set($conn, NULL, NULL,"D:\study\DigiCertGlobalRootCA.crt",NULL,NULL);
+      mysqli_real_connect($conn, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
 
+      if (mysqli_connect_errno($conn)) {
+          die('Failed to connect to MySQL: '.mysqli_connect_error());
       }
       
-  
-      if (mysqli_query($conn,'  
-           CREATE TABLE account (
-          `User` VARCHAR(50) NOT NULL  ,
-          `Password` VARCHAR(50) NOT NULL ,
-          `id INT(30) NOT NULL  `
-          
-          PRIMARY KEY (`User`)
-          );
-      ')) {
-      printf("Table created\n");
+      if (mysqli_query($conn,
+        'CREATE TABLE account (`User` VARCHAR(50) NOT NULL  ,`Password` VARCHAR(50) NOT NULL ,`id INT(30) NOT NULL`PRIMARY KEY (`User`));
+        '
+      )) {
+        printf("Table created\n");
       }
-      
-      mysqli_close($conn);
+        mysqli_close($conn);
 
 ?>
 
