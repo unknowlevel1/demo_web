31a87f723228cb07690c634b10adcf34054ec2b9
unknowlevel1 <matthewnino81@gmail.com>
2025-12-26 14:23:36 -0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,13 +1,13 @@
 <?php
 
-   $host = 'webpractice-server.mysql.database.azure.com';
-   $username = 'brksyjredx';
-   $password = 'SeLfJTxN9ki2W$FY';
-   $db_name = 'webpractice-database';
-   
+
+    $host = "websdemo.mysql.database.azure.com";
+    $username = "zztimdbvxt";
+    $password = "sensen07@";
+    $db_name = "websdemodata";
       
    $conn = mysqli_init();
-   mysqli_ssl_set($conn, NULL, NULL,"D:\study\DigiCertGlobalRootCA.crt.pem",NULL,NULL);
+   mysqli_ssl_set($conn, NULL, NULL,"c:\cert\DigiCertGlobalRootG2.crt.pem",NULL,NULL);
    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
    if (mysqli_connect_errno($conn)) {
    die('Failed to connect to MySQL: '.mysqli_connect_error());
@@ -17,7 +17,7 @@
    $Password= '';
    $id='';
    
-   if($stmt = mysqli_prepare($conn, "INSERT INTO acount (Id,username, password) VALUES (? , ? , ?)")){
+   if($stmt = mysqli_prepare($conn, "INSERT INTO acount (username, password) VALUES (? , ? , ?)")){
        mysqli_stmt_bind_param($stmt, 'ssd',$User,$Password,$id);
        mysqli_stmt_execute($stmt);
        printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
