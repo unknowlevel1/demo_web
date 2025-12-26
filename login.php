f6cd6e3f122f051ffa1ff76958dca2c6e467bce1
unknowlevel1 <matthewnino81@gmail.com>
2025-12-26 09:04:05 -0900

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,12 @@
 <?php
        session_start();
 
-        $host = '';
-        $username = '';
-        $password = '';
-        $db_name = '';
+       $host = "webpractice-server.mysql.database.azure.com";
+       $username = "brksyjredx";
+       $password = "SeLfJTxN9ki2W$FY";
+       $database = "webpractice-database";
 
         $conn = new mysqli($host, $username, $password, $database);
-   
    
         if ($conn->connect_error) {
            die("error" . $conn->connect_error);
@@ -15,15 +14,17 @@
            echo ("connected");
         }
    
-   
        $User = $_POST["username"];
        $Password = $_POST["Password"];
 
-   
+
        $Input_user = mysqli_real_escape_string($conn, $User);
        $Input_pass = mysqli_real_escape_string($conn, $Password);
-   
-       $sql = "SELECT * FROM `account` WHERE username = '$Input_user' and Password = '$Input_pass'";
+
+
+
+       $sql = "SELECT * FROM `account` WHERE username = '$Input_user' and password = '$Input_pass'";
+
       
        $result = mysqli_query($conn, $sql);
        ini_set('display_errors', '0');
