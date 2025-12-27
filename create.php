a950bb3b2c54d146831bf1cfb2fa91695d2462f4
unknowlevel1 <matthewnino81@gmail.com>
2025-12-26 20:04:54 -0900

    Update create.php

--- create.php
+++ create.php
@@ -1,15 +1,16 @@
 <?php
 
    //echo $username = '';
-    $host = "webpractice-server.mysql.database.azure.com";
-    $username = "brksyjredx";
-    $password = "SeLfJTxN9ki2W$FY";
-    $database = "webpractice-database";
+     $host = "webpractice-server.mysql.database.azure.com";
+     $username = "brksyjredx";
+     $password = "SeLfJTxN9ki2W$FY";
+     $database = "webpractice-database";
       
-      $conn = mysqli_init();
+     $conn = mysqli_init();
 
-      mysqli_ssl_set($conn, NULL, NULL,"D:\study\DigiCertGlobalRootCA.crt",NULL,NULL);
+      mysqli_ssl_set($conn, NULL, NULL,"C:/cert/DigiCertGlobalRootG2.crt.pem",NULL,NULL);
       mysqli_real_connect($conn, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
+
 
       if (mysqli_connect_errno($conn)) {
           die('Failed to connect to MySQL: '.mysqli_connect_error());
